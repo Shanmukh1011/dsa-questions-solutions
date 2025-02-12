c64371eaa93a22e29d6f4c89078a76342e7d70a4
Shanmukh1011 <shanmukhm.learning@gmail.com>
2025-02-12 09:10:38 +0530

    Create mergSort.java
    called as depth 1st t = O(n log n), s = O(n);

---
+++ mergSort.java
@@ -0,0 +1,56 @@
+public class Code{
+    public static void printArr(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+    public static void mergSort(int arr[],int si,int ei){
+        if (si >= ei) {
+            return;
+        }
+        //kaam
+        int mid = si+(ei-si)/2;//si+ei/2..
+        mergSort(arr,si,mid);
+        mergSort(arr,mid+1,ei);
+        merge(arr,si,mid,ei);
+        
+       
+    }
+    public static void merge(int arr[],int si,int mid,int ei){
+        //left(0,3)=4 and right(4,6) = 3 -> (6-0+1)= 7;
+        int temp[] = new int[ei-si+1];
+        int i =si;//iterator for left part;
+        int j = mid+1;//iterator for right part
+        int k = 0;//iterator for temparr
+
+        while(i <=mid&& j <= ei){
+            if(arr[i]<=arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+    
+        }
+        //copy remaining elements of left part
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+        //copy temp to original array
+        for(k = 0, i = si ;k <=temp.length-1;i++,k++){
+            arr[i]= temp[k];
+        }
+        
+
+    }
+    public static void main(String[] args){
+    int arr [] = {6,3,9,5,2,8};
+    mergSort(arr,0,arr.length-1);
+    printArr(arr);
+    }
+}
