af1b84e777bef8d2ae5b28b085ae9aaf525a3e0c
Shanmukh1011 <shanmukhm.learning@gmail.com>
2025-02-12 06:48:59 +0530

    Create printsubSets.java
    recursive way to print subsets using list of lists

---
+++ printsubSets.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class Code {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+        List<List<Integer>> result = new ArrayList<>();
+
+        sS(arr, result, new ArrayList<>(), 0);
+        System.out.println(result);
+
+    }
+
+    public static void sS(int arr[], List<List<Integer>> result, List<Integer> subSet, int i) {
+        if(i==3){
+           result.add(new ArrayList<>(subSet));
+            return;
+        }
+        sS(arr, result, subSet, i + 1);
+        subSet.add(arr[i]);
+        sS(arr, result, subSet, i + 1);
+        subSet.remove(subSet.size()-1);
+        
+        
+        
+    }
+}
