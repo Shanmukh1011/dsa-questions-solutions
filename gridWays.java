d30c62dc8c927f270f3fc7baf1577b6e3629a6a8
Shanmukh1011 <shanmukhm.learning@gmail.com>
2025-02-20 13:12:02 +0530

    Create gridWays.java

---
+++ gridWays.java
@@ -0,0 +1,17 @@
+class Code{
+    public static int gridWays(int i,int j,int n,int m){
+        if(i == n-1&&j == m-1){
+            return 1;//base case
+        }else if(i==n||j==m){
+                return 0;//boundry crosses condition
+            }
+            int w1 = gridWays(i+1,j,n,m);
+            int w2 = gridWays(i,j+1,n,m);
+            return w1+w2;
+        
+    }
+    public static void main(String args[]){
+        int n=3,m=3;
+        System.out.println("no of ways = "+gridWays(0, 0, n, m));
+    }
+}
