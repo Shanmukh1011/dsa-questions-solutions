df91a74dbb86bfea8eb7003d20b077e9249a9f27
Shanmukh1011 <shanmukhm.learning@gmail.com>
2025-02-23 04:14:53 +0530

    Update linkedListBasics.java
    added iscycle and remove cycle code

--- linkedListBasics.java
+++ linkedListBasics.java
@@ -212,6 +212,53 @@ public class LinkedList {
         return true;
     }
 
+    public static  boolean isCycle(){
+        Node fast = head;
+        Node slow = head;
+
+        while(fast != null&& fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void removeCycle(){//code does not work for corner case..when last node connects to head;
+        //detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow  = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                cycle = true;
+                break;
+
+            }
+
+        }
+        if(cycle == false){
+            return ;
+        }
+
+
+        //find meeting point;
+        slow = head;
+        Node prev = null;
+        while(slow!=fast){
+            slow = slow.next;
+            prev = fast;
+            fast = fast.next;
+        }
+
+        //remove cycle ->last.next = null;
+        prev.next = null;//last node;
+    }
+
+
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         
